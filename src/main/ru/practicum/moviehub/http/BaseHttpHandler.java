ab19711bf1e71cde714cc783eaa62da6d2b407e8
Nazar Shmigelskiy <nazarshmigelskiy@yandex.ru>
2026-05-22 05:42:42 +0300

    Перенес создание Gson в базовый класс, за инфу про DisplayName спасибо, буду иметь в виду

--- src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
+++ src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
@@ -11,6 +11,7 @@ import java.nio.charset.StandardCharsets;
 abstract class BaseHttpHandler implements HttpHandler {
     protected static final String CT_JSON = "application/json; charset=UTF-8";
     protected final Gson gson = new Gson();
+
     protected void sendJson(HttpExchange ex, int status, String json) throws IOException {
 
         byte[] bytes = json.getBytes(StandardCharsets.UTF_8);
